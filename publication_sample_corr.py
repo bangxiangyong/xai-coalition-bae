0ed066f5b7a87769ce30a1a13aa4c6bc94c43e73
BANG XIANG YONG <yongbangxiang@gmail.com>
2021-04-30 11:03:18 +0100

    fix publication sample correlation figures

--- publication_sample_corr.py
+++ publication_sample_corr.py
@@ -1,64 +1,145 @@
-from analyse_result_helper import load_bae_results
+import os
+import dill
 
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+from matplotlib.cm import ScalarMappable
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+from scipy.stats import pearsonr
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.expand_frame_repr', False)
+pd.set_option('max_colwidth', -1)
 
 
-main_filename = "MLEXP-Explainability/unsupervised-BAE.p"
-publication_folder = "publication/"
-df_perf = load_bae_results(filename = main_filename)
+def plot_correlation_nll(traces_dict, figsize=(8,5)):
 
+    nll_mean_drift = traces_dict['nll-mean-drift']
+    nll_mean_nondrift = traces_dict['nll-mean-nondrift']
 
-# parameters
-dataset = "ZEMA"
-threshold_pearson = 0.9
+    if 'nll-var-drift' in list(traces_dict.keys()):
+        has_var = True
+    else:
+        has_var = False
 
-# start filter dfs
-filtered_df = df_perf[df_perf["dataset"] == dataset]
-df_bae_central = filtered_df[(filtered_df["bae_or_ae"] == "bae")
-                                      & (filtered_df["bae_config"] == "central")
-                                      & (filtered_df["perf_name"] == "pearson")
-                                      & (filtered_df["perf_score"] > threshold_pearson)
-]
+    y_target = np.linspace(0.25,0.95,nll_mean_drift.shape[0])
 
+    nll_mean_perturbed = nll_mean_drift
+    nll_mean_unperturbed = nll_mean_nondrift
 
+    if has_var:
+        nll_var_perturbed = traces_dict['nll-var-drift']
+        nll_var_unperturbed = traces_dict['nll-var-nondrift']
 
-df_bae_coalition = filtered_df[(filtered_df["bae_or_ae"] == "bae")
-                                      & (filtered_df["bae_config"] == "coalition")
-                                      & (filtered_df["perf_name"] == "pearson")
-]
+        pearson_corr_1 = pearsonr(nll_var_perturbed, nll_var_unperturbed)[0]
 
-select_row_central = df_bae_central.iloc[0]
-select_row_coalition = df_bae_coalition[
-    (df_bae_coalition["total_model_cap"] == select_row_central["total_model_cap"]) &
-    (df_bae_coalition["perturbed"] == select_row_central["perturbed"]) &
-    (df_bae_coalition["axis"] == select_row_central["axis"])
-]
-print(select_row_central["encode"])
-print(select_row_coalition["encode"].unique()[0])
+    pearson_corr_0 = pearsonr(nll_mean_perturbed, nll_mean_unperturbed)[0]
 
-pairs = {"central":[],"coalition":[]}
-for encode in list(df_bae_central["encode"].unique()):
-    select_row_central = df_bae_central[df_bae_central["encode"]==encode].iloc[0]
-    select_row_coalition = df_bae_coalition[
-        (df_bae_coalition["total_model_cap"] == select_row_central["total_model_cap"]) &
-        (df_bae_coalition["perturbed"] == select_row_central["perturbed"]) &
-        (df_bae_coalition["axis"] == select_row_central["axis"])
-    ].iloc[0]
-    pairs["central"].append(select_row_central["encode"])
-    pairs["coalition"].append(select_row_coalition["encode"])
-pairs = pd.DataFrame(pairs)
-pd.set_option('display.max_rows', 500)
-print(pairs)
+    # actual plotting
+    nondrift_color = "tab:orange"
+    drift_color = "tab:blue"
 
+    fig, axes = plt.subplots(2,2, figsize=figsize)
+    axes= axes.flatten()
 
+    # nll_perturbed
+    axes[0].plot(y_target, nll_mean_unperturbed, color=nondrift_color)
+    axes_0_twin = axes[0].twinx()
+    axes_1_twin = axes[1].twinx()
+    axes_0_twin.plot(y_target, nll_mean_perturbed, color=drift_color)
 
+    if has_var:
+        axes[1].plot(y_target, nll_var_unperturbed,  color=nondrift_color)
+        axes_1_twin.plot(y_target, nll_var_perturbed, color=drift_color)
+        axes[3].scatter(nll_var_perturbed, nll_var_unperturbed, c=y_target)
 
+    # bottom plots
+    axes[2].scatter(nll_mean_perturbed, nll_mean_unperturbed, c=y_target)
 
+    # set labels symbols
+    nondrift_text = r"$S^{\bot{shift}}$"
+    drift_text = r"$S^{shift}$"
+    e_nll_text = r"$\mathrm{E_{\theta}(NLL)}$"
+    var_nll_text = r"$\mathrm{Var_{\theta}(NLL)}$"
 
+    # set labels
+    axes[0].set_ylabel(e_nll_text+","+nondrift_text, color=nondrift_color)
+    axes_0_twin.set_ylabel(e_nll_text+","+drift_text, color=drift_color)
+    axes[1].set_ylabel(var_nll_text+","+nondrift_text, color=nondrift_color)
+    axes_1_twin.set_ylabel(var_nll_text+","+drift_text, color=drift_color)
+    axes[0].tick_params(axis='y', labelcolor=nondrift_color)
+    axes_0_twin.tick_params(axis='y', labelcolor=drift_color)
+    axes[1].tick_params(axis='y', labelcolor=nondrift_color)
+    axes_1_twin.tick_params(axis='y', labelcolor=drift_color)
 
+    # axes[0].set_xticks([0.25, 0.35, 0.45, 0.55,0.65,0.75,0.85,0.95])
+    # axes[1].set_xticks([0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 0.85, 0.95])
+    axes[0].set_xticks([0.25, 0.50, 0.75, 0.95])
+    axes[1].set_xticks([0.25, 0.50, 0.75, 0.95])
 
+    axes[0].set_xlabel("Degradation")
+    axes[1].set_xlabel("Degradation")
 
+    axes[2].set_xlabel(e_nll_text+","+drift_text)
+    axes[2].set_ylabel(e_nll_text+","+nondrift_text)
 
+    axes[3].set_xlabel(var_nll_text+","+drift_text)
+    axes[3].set_ylabel(var_nll_text+","+nondrift_text)
+
+    axes[2].text(.5, .9, "Pearson: %.2f" % pearson_corr_0,
+            horizontalalignment='center',
+            transform=axes[2].transAxes)
+
+    if has_var:
+        axes[3].text(.5, .9, "Pearson: %.2f" % pearson_corr_1,
+                horizontalalignment='center',
+                transform=axes[3].transAxes)
+
+    # set color bar
+    norm = plt.Normalize(y_target.min(), y_target.max())
+    cmap = plt.get_cmap("viridis")
+    sm = ScalarMappable(norm=norm, cmap=cmap)
+    sm.set_array([])
+
+    axins = inset_axes(axes[3],
+                       width="5%",  # width = 5% of parent_bbox width
+                       height="98%",  # height : 50%
+                       loc='lower left',
+                       bbox_to_anchor=(1.05, 0., 1, 1),
+                       bbox_transform=axes[3].transAxes,
+                       borderpad=0,
+                       )
+
+    cbar = fig.colorbar(sm, cax=axins, ticks=[0.25, 0.50, 0.75, 0.95])
+
+    plt.tight_layout()
+    return fig
+
+# load dill and plot
+encodes = ["390ef039e07d26d77c525c258e37f43f7a7700.p",
+"96241a6129e9717884aa61338475aa228971.p",
+"8ea85cad5702975aaed0a3cc1112076121d83c.p",
+"a3ce2c815decfed70ad14f74666c82c1de7.p"
+ ]
+
+ml_exp_folder = "MLEXP-Explainability/"
+
+# handle figure output folder
+output_folder = "publication/"
+output_prefixes = ["pronostia-bae-central-sample-",
+                   "pronostia-bae-coalition-sample-",
+                   "zema-bae-central-sample-",
+                   "zema-bae-coalition-sample-"
+                   ]
+if not os.path.exists(output_folder):
+    os.mkdir(output_folder)
+
+# save figure
+for encode,output_prefix in zip(encodes,output_prefixes):
+    traces_dict = dill.load(open(ml_exp_folder + encode, "rb"))
+    fig = plot_correlation_nll(traces_dict, figsize=(8,5))
+    fig.savefig(output_folder+output_prefix+encode.split('.p')[0]+".png", dpi=500)
 
 
 
